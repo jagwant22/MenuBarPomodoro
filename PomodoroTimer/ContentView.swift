1214ff95f630bced0c693431b6a92c1cddeab713
Jagwant Sehgal <sehgal.jagwant@gmail.com>
2021-03-24 11:37:35 -0400

    Initial Commit

---
+++ PomodoroTimer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PomodoroTimer
+//
+//  Created by Jagwant Sehgal on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
